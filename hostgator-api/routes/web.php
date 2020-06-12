3904c579e05425861a82dcf80d803d6703f336f3
Elisson Alves Silva <elisson@MacBook-Pro-de-Elisson.local>
2020-06-12 00:28:16 -0400

    refact: rename url

--- hostgator-api/routes/web.php
+++ hostgator-api/routes/web.php
@@ -5,6 +5,6 @@ use App\Controllers\ProductsController;
 
 $app->get('/health', HealthController::class . ':health');
 
-$app->get('/api/products', ProductsController::class . ':index');
-$app->get('/api/products/{id}', ProductsController::class . ':getProduct');
+$app->get('/api/prices', ProductsController::class . ':index');
+$app->get('/api/prices/{id}', ProductsController::class . ':getProduct');
 
