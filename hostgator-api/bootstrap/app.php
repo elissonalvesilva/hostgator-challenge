708e196623722e55cda93c27f100589ad2a449d2
Elisson Alves Silva <elisson@MacBook-Pro-de-Elisson.local>
2020-06-11 23:57:51 -0400

    feat: add error handler

--- hostgator-api/bootstrap/app.php
+++ hostgator-api/bootstrap/app.php
@@ -2,14 +2,17 @@
 
 use DI\Container;
 use Dotenv\Dotenv;
-use Slim\Views\Twig;
 use Slim\Factory\AppFactory;
-use Slim\Views\TwigExtension;
-use Slim\Psr7\Factory\UriFactory;
 use Dotenv\Exception\InvalidPathException;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Log\LoggerInterface;
+use Slim\Psr7\Response;
+use Slim\Psr7\Request;
+
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
+// load .env
 try {
     (new Dotenv(__DIR__ . '/../'))->load();
 } catch (InvalidPathException $e) {
@@ -22,6 +25,12 @@ AppFactory::setContainer($container);
 
 $app = AppFactory::create();
 
+// Add Routing Middleware
+$app->addRoutingMiddleware();
+
+/**
+ * Add settings in container
+ */
 $container->set('settings', function () {
     return [
         'app' => [
@@ -37,6 +46,29 @@ $container->set('settings', function () {
         ],
     ];
 });
+
+
+$customErrorHandler = function (
+    ServerRequestInterface $request,
+    \Throwable $exception,
+    bool $displayErrorDetails,
+    bool $logErrors,
+    bool $logErrorDetails
+) use ($app) {
+    $response = $app->getResponseFactory()->createResponse();
+    $payload = json_encode(['error' => 'Not found Route']);
+    $response->getBody()->write($payload);
+    return $response
+            ->withHeader('Content-Type', 'application/json')
+            ->withStatus(400);
+};
+
+// Add Error Middleware
+$errorMiddleware = $app->addErrorMiddleware(true, true, true);
+// Register the handler to handle only  HttpNotFoundException
+// Changing the first parameter registers the error handler for other types of exceptions
+$errorMiddleware->setErrorHandler(Slim\Exception\HttpNotFoundException::class, $customErrorHandler);
+
 
 require_once __DIR__ . '/../bootstrap/database.php';
 require_once __DIR__ . '/../routes/web.php';
