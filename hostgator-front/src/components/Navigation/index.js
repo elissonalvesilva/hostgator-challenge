b4dc5642e513cc15dee13b4d143628c0bdb8858e
Elisson Alves Silva <elisson@MacBook-Pro-de-Elisson.local>
2020-06-12 15:57:30 -0400

    feat: make a frontend

---
+++ hostgator-front/src/components/Navigation/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import hostgator from '../../../images/hostgator.svg';
+
+function Navigation() {
+  return (
+    <div className="nav">
+      <div className="nav-content">
+        <img src={hostgator} alt="hostgator logo" />
+      </div>
+    </div>
+  );
+}
+
+export default Navigation;
---
+++ hostgator-front/webpack.config.js
@@ -0,0 +1,57 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  entry: {
+    app: './src/index.js',
+  },
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    publicPath: '/',
+    filename: 'bundle.js',
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './build',
+    hot: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: 'eslint-loader',
+        options: {
+          configFile: path.resolve(__dirname, '.eslintrc'),
+        },
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          'style-loader',
+          // Translates CSS into CommonJS
+          'css-loader',
+          // Compiles Sass to CSS
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ['file-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: path.resolve('./public/index.html'),
+    }),
+  ],
+};
