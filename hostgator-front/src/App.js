e0a50753ded9fb07e98de7e4f4fe3330fd2047e8
Elisson Alves Silva <elisson@MacBook-Pro-de-Elisson.local>
2020-06-12 18:06:10 -0400

    feat: make a header and navigation component

--- hostgator-front/src/App.js
+++ hostgator-front/src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import Navigation from './components/Navigation';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
-      <Navigation />
+      <Header />
     </div>
   );
 }
--- hostgator-front/src/components/Navigation/index.js
+++ hostgator-front/src/components/Navigation/index.js
@@ -4,8 +4,8 @@ import hostgator from '../../../images/hostgator.svg';
 function Navigation() {
   return (
     <div className="nav">
-      <div className="nav-content">
-        <img src={hostgator} alt="hostgator logo" />
+      <div className="nav__content">
+        <img src={hostgator} alt="hostgator logo" className="nav__logo" />
       </div>
     </div>
   );
