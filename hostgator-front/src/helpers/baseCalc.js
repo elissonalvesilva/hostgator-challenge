1936b32d30834b334772376156bc7974bba0bb25
Elisson Alves Silva <elisson@MacBook-Pro-de-Elisson.local>
2020-06-14 16:10:43 -0400

    feat: add commenters is helper base calc

--- hostgator-front/src/helpers/baseCalc.js
+++ hostgator-front/src/helpers/baseCalc.js
@@ -1,10 +1,30 @@
 const calc = {
+  /**
+   * calculate a discount
+   * @param Double originalValue - original value
+   * @param Double discount - discount
+   * @returns Double discount result
+   */
   calcDiscount(originalValue, discount = 0.4) {
     return originalValue - (originalValue * discount);
   },
+
+  /**
+   * calculate installments by cycle
+   * @param Double valueWithDiscount - value with discount
+   * @param Double installments - installments
+   * @returns Double total installments value
+   */
   calcCycleInstallments(valueWithDiscount, installments) {
     return valueWithDiscount / installments;
   },
+
+  /**
+   * calculate offer
+   * @param Double originalValue - original value
+   * @param Double valueWithDiscount - value without discount
+   * @returns offer
+   */
   offer(originalValue, valueWithDiscount) {
     return originalValue - valueWithDiscount;
   },
